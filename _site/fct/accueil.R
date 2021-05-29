2de128e14e0e0922c321a9b6f94b8101e4504fca
mell421 <43302304+mell421@users.noreply.github.com>
2021-05-29 16:03:52 +0200

    update fct

---
+++ _site/fct/accueil.R
@@ -0,0 +1,17 @@
+# devtools::document("ajoutRep")
+# install.packages("gsheet")
+
+# usethis::use_vignette("base_de_donnees_principale")
+
+
+# fonction qui charge la page d'accueil
+accueil <- function(){
+  table <- suppressWarnings(gsheet2tbl('https://docs.google.com/spreadsheets/d/1HZydd6qo-GcQVqnwQnerHHnMEyR6faJXKN7Egkuqp4I/edit#gid=1775525986'))
+  table <- table[1:15]
+  na.omit(table)
+  table <- data.frame(table)
+
+  return(table)
+}
+s <- accueil()
+head(s)
---
+++ _site/fct/sources.R
@@ -0,0 +1,42 @@
+# devtools::document("ajoutRep")
+# install.packages("gsheet")
+# usethis::use_vignette("base_de_donnees_principale")
+# devtools::document("ajoutRep")
+# devtools::install("ajoutRep")
+
+loadMesSources <- function(){
+  source("~/GitHub/monSiteR/fct/accueil.R")
+  source("~/GitHub/monSiteR/fct/copy.R")
+  source("~/GitHub/monSiteR/fct/nombre.R")
+  source("~/GitHub/monSiteR/fct/max.resume.R")
+  source("~/GitHub/monSiteR/fct/resume.R")
+  source("~/GitHub/monSiteR/fct/page.R")
+}
+installMesPackages <- function(){
+  install.packages("gsheet")
+  install.packages("tm")
+  install.packages("SnowballC")
+  install.packages("wordcloud")
+  install.packages("RColorBrewer")
+  install.packages("syuzhet")
+  install.packages("ggplot2")
+  install.packages(utf8)
+  install.packages(dplyr)
+}
+
+
+
+loadMesPackages <- function(){
+  library("gsheet")
+  library("tm")
+  library("SnowballC")
+  library("wordcloud")
+  library("RColorBrewer")
+  library("syuzhet")
+  library("ggplot2")
+  library(utf8)
+  # library(dplyr)
+}
+
+loadMesPackages()
+loadMesSources()
